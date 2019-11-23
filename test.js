c7efb8d1e9d749bcdd10f8d5a90e1dd5428b9ec1
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2019-11-23 08:45:14 -0600

    update test.js

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
 var cd = require ('./chilldown')
 
 
-var markdown = `jrjr
-rjjr
+var markdown = `
 #helloworld
 how is everyone doing today?
+
+##helloworld
+another paragraph
+
+###list
+- one
+- two
+- three
+
+
 
 `
 console.log(cd.downToHTML(markdown))
